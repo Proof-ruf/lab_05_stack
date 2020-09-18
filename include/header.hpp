cb9a179bb1349ec65d868fc70ab6f59197907869
unknown <Tzybulko.i@inbox.ru>
2020-09-18 05:12:39 +0300

    first commit

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,97 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#define ERROR -1
+
+template <typename T>
+class Stack{
+ public:
+  explicit Stack(int s) : ArrStack(nullptr){
+    size = s;
+    top = 0;
+  }
+
+  ~Stack() {
+    free(ArrStack);
+  }
+
+  void push(T&& value){
+    top++;
+    ArrStack = static_cast<T*>(realloc(ArrStack, top * sizeof(T)));
+    ArrStack[top - 1] = value;
+  }
+
+  void push(const T& value){
+    top++;
+    ArrStack = static_cast<T*>(realloc(ArrStack, top * sizeof(T)));
+    ArrStack[top - 1] = value;
+  }
+
+  void pop(){
+    if (top == 0) {
+      exit(ERROR);
+    }
+    --top;
+    ArrStack = static_cast<T*>(realloc(ArrStack, top * sizeof(T)));
+  }
+
+  const T& head() const{
+    return ArrStack[top-1];
+  }
+
+ private:
+  T *ArrStack;
+  int top;
+  int size;
+};
+
+template <typename T>
+class stack{
+ public:
+
+  explicit stack(int s) : ArrStack(nullptr){
+    size = s;
+    top = 0;
+  }
+
+  ~stack(){
+    free(ArrStack);
+  }
+  template <typename ... Args>
+  void push_emplace(Args&&... value){
+    T obj(std::forward<Args>(value)...);
+    top++;
+    ArrStack = static_cast<T*>(realloc(ArrStack, top * sizeof(T)));
+    ArrStack[top - 1] = obj;
+  }
+
+  void push(T&& value){
+    top++;
+    ArrStack = static_cast<T*>(realloc(ArrStack, top * sizeof(T)));
+    ArrStack[top - 1] = value;
+  }
+
+  const T& head() const{
+    return ArrStack[top-1];
+  }
+
+  T pop(){
+    if (top == 0) {
+      exit(ERROR);
+    }
+    T rezult = ArrStack[top - 1];
+    --top;
+    ArrStack = static_cast<T*> (realloc(ArrStack, top * sizeof(T)));
+    return rezult;
+  }
+
+ private:
+  T *ArrStack;
+  int top;
+  int size;
+};
 
 #endif // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,55 @@
 // Copyright 2018 Your Name <your_email>
 
-#include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include <gtest/gtest.h>
+#include <header.hpp>
+#include <type_traits>
+
+
+class Length{
+ public:
+  int sum;
+  Length() {}
+  Length(std::string S1, std::string S2){
+    sum = S1.length() + S2.length();
+  }
+  explicit Length(std::string S1){
+    sum = S1.length();
+  }
+};
+
+TEST(one, EmptyTest) {
+Stack<int> Stack_1(30);
+int var_1 = 6;
+int var_2 = 8;
+int var_3 = 10;
+Stack_1.push(var_1);
+Stack_1.push(var_2);
+Stack_1.pop();
+Stack_1.push(var_3);
+EXPECT_EQ(static_cast<int>(Stack_1.head()), 10);
 }
+
+
+TEST(two, EmptyTest) {
+Length obj("Malo");
+stack<Length> Stack_2(30);
+Stack_2.push_emplace("Halo", "Dasha");
+Stack_2.push(std::move(obj));
+EXPECT_EQ(Stack_2.pop().sum, 4);
+}
+
+
+TEST(three, EmptyTest) {
+EXPECT_EQ(std::is_move_constructible<Stack<int>>::value, true);
+EXPECT_EQ(std::is_move_assignable<Stack<int>>::value, true);
+EXPECT_EQ(std::is_move_constructible<stack<int>>::value, true);
+EXPECT_EQ(std::is_move_assignable<stack<int>>::value, true);
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
